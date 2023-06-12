336a8d85a7e94d359e9b8f41a91b13fae00e7ed7
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-12 17:46:58 +0530

    Add project files.

---
+++ CHAPTER 05/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 5. WAP to accept a number from the user & check number is +ve or -ve or zero ?
+
+using System.ComponentModel.Design;
+
+Console.WriteLine("enter a number");
+
+int number  = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    Console.WriteLine("Number is positive");
+}
+else if (number < 0)
+{
+    Console.WriteLine("Number is negative");
+}
+else if (number == 0)
+{
+    Console.WriteLine("Number is equal to zero");
+}
+else
+{
+    Console.WriteLine(" ");
+}
+
+
+
